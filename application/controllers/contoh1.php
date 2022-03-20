b3755fd7232acccc3bddf63abd22a45932accc1a
WP2Rafli <rafliieu73@gmail.com>
2022-03-21 00:52:23 +0700

    wp2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h1> perkenalkan </h1>";
+        echo "nama saya Muhamad Rafli
+            saya tingal didaerah Bogor
+            olahraga yang saya sukai adalah
+            badminton";
+	}
+}
